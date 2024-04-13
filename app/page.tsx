95dbc03a474cfc479a265fc68211fb65af898196
Samiul Huque <Samiul.Huque@gmail.com>
2024-04-13 19:35:10 -0400

    vertically aligned content

--- app/page.tsx
+++ app/page.tsx
@@ -41,8 +41,9 @@ function calculateFutureValueCompoundInterest(price: number, interestRate: numbe
   return (
     <div>
       <h1>What&apos;s it in retirement?</h1>
+      {isVIsible && (<h2 className="text-gray-600 mb-4">Your investment will be worth {futurePrice} in retirement</h2>)}
       <div className="flex justify-center items-center h-screen">
-        <form className="flex justify-center items-center h-screen" onSubmit={handleSubmit}>
+        <form className="w-full max-w-md bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2">
               How much is it?: <br></br>
@@ -66,7 +67,7 @@ function calculateFutureValueCompoundInterest(price: number, interestRate: numbe
             </label><br></br>
           </div>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Submit</button>
-        {isVIsible && (<p className="text-sm text-gray-600 mb-4">Your investment will be worth {futurePrice} in retirement</p>)}
+        {/* {isVIsible && (<p className="text-sm text-gray-600 mb-4">Your investment will be worth {futurePrice} in retirement</p>)} */}
       </form>
     </div>
     </div>
